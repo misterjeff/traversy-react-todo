5c3e3b294d49713c457d4721db610689e14fe713
Jeffrey Brown <jbrown@aeci.org>
2020-02-04 14:47:29 -0600

    Add About component, react router, etc.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 // Traversy - React JS Crash Course (https://www.youtube.com/watch?v=sBws8MSXN7A)
-// Stopped at 1:08:24
+// Stopped at 1:31:00
 
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About'
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
-      {
-        id: 1,
-        title: 'Take out trash',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Walk dog',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Grocery shop',
-        completed: false
-      }
+      // {
+      //   id: uuid.v4(),
+      //   title: 'Take out trash',
+      //   completed: false
+      // },
+      // {
+      //   id: uuid.v4(),
+      //   title: 'Walk dog',
+      //   completed: false
+      // },
+      // {
+      //   id: uuid.v4(),
+      //   title: 'Grocery shop',
+      //   completed: false
+      // }
     ]
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+      .then(res => this.setState({ todos: res.data }))
   }
 
   // Toggle completion of to-do item
   markComplete = (id) => {
     this.setState({ todos: this.state.todos.map((todo) => {
-      if(todo.id == id) {
+      if(todo.id === id) {
         todo.completed = !todo.completed
       }
       return todo;
     }) });
+  }
+
+  // Add to-do item
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
   }
 
   // Delete to-do item
@@ -45,13 +64,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
